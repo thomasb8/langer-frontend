e7f1ae4384ab837c8ddb547094b35a6c17141220
Tamás Batka <tamas.batka@pepperlabs.io>
2022-11-22 00:37:03 +0100

    Set up build for deployment

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,3 +1,7 @@
 import axios from "axios";
 
-export default axios.create({ baseURL: 'http://localhost:3001/',  withCredentials: true })
+export default axios.create({
+  baseURL: import.meta.env.VITE_LANGER_BACKEND_URL,
+  withCredentials: true,
+  headers: { 'Content-Type': 'application/json' }
+})
